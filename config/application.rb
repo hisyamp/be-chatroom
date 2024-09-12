c72ff3e87d9d423224e5f9f1c602e60ae7364458
hisyamp <hisyampriaji.hp@gmail.com>
2024-09-12 23:39:54 +0700

    final code 4

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,11 @@
-Dotenv::Railtie.load
-
 require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
-# require "active_record/railtie"
-# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
@@ -23,29 +17,12 @@ Bundler.require(*Rails.groups)
 module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-   config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'  # Allows all origins
-    
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head],
-          credentials: false  # You must set this to false with origins '*'
-      end
+    # Load Dotenv only in development or test environment
+    if Rails.env.development? || Rails.env.test?
+      Dotenv::Railtie.load
     end
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Other configurations...
   end
 end
