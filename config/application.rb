01b5dc4d36f9d6cc021fd1e7d6d690174a65fa7e
hisyamp <hisyampriaji.hp@gmail.com>
2024-09-12 12:45:22 +0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,14 +29,15 @@ module App
     config.autoload_lib(ignore: %w(assets tasks))
    config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*' # Adjust the origin as needed
-
+        origins '*'  # Allows all origins
+    
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
-          credentials: true
+          credentials: false  # You must set this to false with origins '*'
       end
     end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
